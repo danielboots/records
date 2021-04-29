c9d76fa992717f3743fd0c06fff4d463f3c9c70c
danielboots <you@example.com>
2021-04-29 14:40:55 +0100

    added key identifier to loop on HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+
 import { Container } from "react-bootstrap";
 
 function App() {
@@ -7,8 +9,9 @@ function App() {
     <div>
       <Header />
       <main className="py-3">
+        <h1 className="text-center">Intimacy Records</h1>
         <Container>
-          <h1 className="text-center">Intimacy Records</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
