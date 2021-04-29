68ca5b1cc4553a7a98bf0f376e9efcbed20dca93
danielboots <you@example.com>
2021-04-29 13:03:45 +0100

    capitalise for components, fixed bug on import

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <Header>Header</Header>
+            this is the header
+        </div> 
+    )
+}
+
+export default Header
